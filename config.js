19bbea220f05297231051ade2920bc49f9d84a25
Niyi-Bankole Ademakinwa <135022572+VoidThoughts888@users.noreply.github.com>
2025-05-28 08:52:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=jtoXgZiQ#wcfEG7Ejz6vTbiBw-3kio3aYex6Bvfm2a9VAFeROJmU",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
